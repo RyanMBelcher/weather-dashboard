5d79d888a13cc46a595826769556853b7e073fa2
Ryan Belcher <ryanbelcher@ryans-air.myfiosgateway.com>
2022-11-20 17:59:03 -0500

    added current forecast logic

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ let APIKey = 'e919e1cf7f216e7a1fc09c0a20305ee4';
 let city = 'philadelphia';
 
 let searchButton = document.getElementById('search-btn');
+let weatherList = document.getElementById('weather-list');
+let forecastSection = document.getElementById('forecast-section');
+let currentCity = document.getElementById('current-city');
 
-function getAPI() {
+function currentForecast() {
 
-    let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}`;
-
-
+    let requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKey}`;
 
     fetch(requestUrl)
         .then(function (response) {
@@ -16,10 +17,28 @@ function getAPI() {
         .then(function (data) {
             console.log(data)
 
-            for (var i = 0; i < data.length; i++) {
+            let = tempItem = document.createElement('li');
+            let = humidityItem = document.createElement('li');
+            let = windItem = document.createElement('li');
 
-            }
-        });
+            tempItem.textContent = ('Temp: ') + data.main.temp + ('° F')
+            humidityItem.textContent = ('Humidity: ') + data.main.humidity + ('%');
+            windItem.textContent = ('Wind Speed: ') + data.wind.speed + (' MPH');
+
+            forecastSection.classList.remove('hidden');
+            weatherList.appendChild(tempItem);
+            weatherList.appendChild(humidityItem);
+            weatherList.appendChild(windItem);
+            currentCity.textContent = data.name;
+
+        }); showDate();
 }
 
-searchButton.addEventListener('click', getAPI);            
+searchButton.addEventListener('click', currentForecast);
+
+function showDate() {
+    let today = dayjs();
+    // This displays the current date
+    $('#current-day').text(today.format('(MMMM D, YYYY)'));
+
+}
