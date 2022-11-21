2db8e0769c5323145fe91a7af4941ee93aa5138f
Ryan Belcher <ryanbelcher@ryans-air.myfiosgateway.com>
2022-11-21 17:05:36 -0500

    finished the five day forecast

--- script.js
+++ script.js
@@ -50,6 +50,22 @@ function fetchData() {
         // This displays the current date
         $('#current-day').text(today.format('(MMMM D, YYYY)'));
 
+        let dayOne = dayjs().add(1, 'days')
+        $('#day-one').text(dayOne.format('MM / D / YYYY'))
+        let dayTwo = dayjs().add(2, 'days')
+        $('#day-two').text(dayTwo.format('MM / D / YYYY'))
+        let dayThree = dayjs().add(3, 'days')
+        $('#day-three').text(dayThree.format('MM / D / YYYY'))
+        let dayFour = dayjs().add(4, 'days')
+        $('#day-four').text(dayFour.format('MM / D / YYYY'))
+        let dayFive = dayjs().add(5, 'days')
+        $('#day-five').text(dayFive.format('MM / D / YYYY'))
+
+        console.log(dayOne)
+        console.log(dayTwo)
+        console.log(dayThree)
+        console.log(dayFour)
+        console.log(dayFive)
     }
 
     function fiveDayForecast() {
@@ -61,30 +77,59 @@ function fetchData() {
                 return response.json();
             })
             .then(function (data) {
+                console.log(data.list.length);
+                // var weatherArr = data.list;
+                let day = 1;
+                for (let i = 0; i < data.list.length; i += 8) {
+                    let tempItem = document.createElement('li');
+                    let humidityItem = document.createElement('li');
+                    let windItem = document.createElement('li');
 
-                let = tempItem = document.createElement('li');
-                let = humidityItem = document.createElement('li');
-                let = windItem = document.createElement('li');
-                console.log(data);
 
-                for (let i = 0; i < data.length; i++); {
+                    tempItem.textContent = ('Temp: ') + data.list[i].main.temp + ('° F')
+                    humidityItem.textContent = ('Humidity: ') + data.list[i].main.humidity + ('%');
+                    windItem.textContent = ('Wind Speed: ') + data.list[i].wind.speed + (' MPH');
 
-                    tempItem.textContent = ('Temp: ') + data.main.temp + ('° F')
-                    humidityItem.textContent = ('Humidity: ') + data.main.humidity + ('%');
-                    windItem.textContent = ('Wind Speed: ') + data.wind.speed + (' MPH');
+                    const currentListItem = document.getElementById(`five-day-list-${day}`)
+                    console.log(currentListItem);
+                    console.log(day);
 
-                    fiveDayList.appendChild(tempItem);
-                    fiveDayList.appendChild(humidityItem);
-                    fiveDayList.appendChild(windItem);
+                    currentListItem.appendChild(tempItem);
+                    currentListItem.appendChild(humidityItem);
+                    currentListItem.appendChild(windItem);
+                    day += 1;
+
                 }
-            }
-            )
+                // weatherArr.forEach((element, b) => {
 
-    }; fiveDayForecast();
+                //     // console.log(index)
+
+
+                // })
+                // var i = 0;
+                // while (i < weatherArr) {
+                //     console.log(i);
+
+                //     tempItem.textContent = ('Temp: ') + data.list[i].main.temp + ('° F')
+                //     humidityItem.textContent = ('Humidity: ') + data.list[i].main.humidity + ('%');
+                //     windItem.textContent = ('Wind Speed: ') + data.list[i].wind.speed + (' MPH');
+
+                //     fiveDayList.appendChild(tempItem);
+                //     fiveDayList.appendChild(humidityItem);
+                //     fiveDayList.appendChild(windItem);
+                //     i += 8
+                // }
+                // }
+                // )
+
+            });
+    }
+
+    fiveDayForecast();
+
+
+    function getGeoCode(city, APIKey) {
+        let geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APIKey}`
+    }
 }
-
-function getGeoCode(city, APIKey) {
-    let geoURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${APIKey}`
-}
-
 searchButton.addEventListener('click', fetchData)
